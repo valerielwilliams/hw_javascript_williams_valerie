49a4d17d6c6403cdd363b74ef51c5261a3469a85
Valerie Williams <valerielwilliams@ufl.edu>
2017-01-29 17:54:31 -0500

    add an array of last five US presidents
    I added the last five US presidents (starting with President Trump)
    as objects. I added those objects to an array and logged out President
    George Bush.(George W, not George HW.)

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var presidents =
+    [
+{firstName: 'Donald', lastName: 'Trump', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '2016 - 2020'},
+{firstName: 'Barack', lastName: 'Obama', termLength: 'Eight Years', party: 'Democrat', yearsOfPresidency: '2008 - 2016'},
+{firstName: 'George', lastName: 'Bush', termLength: 'Eight Years', party: 'Republican', yearsOfPresidency: '2000 - 2008'},
+{firstName: 'Bill', lastName: 'Clinton', termLength: 'Eight Years', party: 'Democrat', yearsOfPresidency: '1993 - 2000'},
+{firstName: 'George', lastName: 'Bush', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '1988 - 1993'},
+    ]
+;
+
+console.log(presidents[2].firstName, presidents[2].lastName);
