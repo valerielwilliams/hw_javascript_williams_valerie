6630612e39139e232677112416c16c928f183a63
Valerie Williams <valerielwilliams@ufl.edu>
2017-01-29 20:09:17 -0500

    add middle initial to each President Bush
    I added W to the name of our 43rd president and HW to the name of our
    41st president for clarification.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ var presidents =
     [
 {firstName: 'Donald', lastName: 'Trump', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '2016 - 2020'},
 {firstName: 'Barack', lastName: 'Obama', termLength: 'Eight Years', party: 'Democrat', yearsOfPresidency: '2008 - 2016'},
-{firstName: 'George', lastName: 'Bush', termLength: 'Eight Years', party: 'Republican', yearsOfPresidency: '2000 - 2008'},
+{firstName: 'George W', lastName: 'Bush', termLength: 'Eight Years', party: 'Republican', yearsOfPresidency: '2000 - 2008'},
 {firstName: 'Bill', lastName: 'Clinton', termLength: 'Eight Years', party: 'Democrat', yearsOfPresidency: '1993 - 2000'},
-{firstName: 'George', lastName: 'Bush', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '1988 - 1993'},
+{firstName: 'George HW', lastName: 'Bush', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '1988 - 1993'},
     ]
 ;
 
