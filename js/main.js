1ebfc2ceb51bc6a65c53d99ce9951d7b61cf24e3
Valerie Williams <valerielwilliams@ufl.edu>
2017-02-01 18:03:28 -0500

    add numba function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ function howdy() {
 }
 
 console.log(howdy());
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little number!');
+    } else if (string.length > 7) {
+        console.log("I'm sorry, but that's too many to count.");
+    } else {
+        console.log('7, what a perfect choice!');
+    }
+
+};
+
+numba('7');
+numba('seventy');
+numba('university');
